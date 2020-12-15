c1b3aa0ee89631e94da3eea77161b2c0b55f748a
pabloa1311 <pabloa1311@gmail.com>
2020-12-15 15:09:26 -0500

    handleChange function added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react"
-import './styles.css';
+import React, {useState} from "react"
+
 
 function App() {
+  const [words, setWords] = useState("")
+  
+  function handleChange(e) {
+      const {value} = e.target
+      setWords(value)
+  }
+  
+  console.log(words)
   return (
-    <div>
-        <h1>How fast do you type?</h1>
-        <textarea />
-        <h4>Time reminaing: ???</h4>
-        <button>Start</button>
-        <h1>Word count: ???</h1>
-    </div>
-  );
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea 
+              value={words}
+              name="words"
+              placeholder="Type in..."
+              onChange={handleChange}
+          />
+          <h4>Time reminaing: ???</h4>
+          <button>Start</button>
+          <h1>Word count: ???</h1>
+      </div>
+  )
 }
 
-export default App;
+export default App
