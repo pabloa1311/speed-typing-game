126af77deaa4737f018de2aba9882a6f94d7988a
pabloa1311 <pabloa1311@gmail.com>
2020-12-16 11:20:15 -0500

    First playable version

--- src/App.js
+++ src/App.js
@@ -2,40 +2,61 @@ import React, {useState, useEffect} from "react"
 
 
 function App() {
-  const [words, setWords] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(5)
+  const STARTING_TIME = 5
+  
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
   
   function handleChange(e) {
       const {value} = e.target
-      setWords(value)
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
+      setText(value)
   }
   
-  useEffect(() => {
-    if(timeRemaining > 0) {
-        setTimeout(() => {
-            setTimeRemaining(time => time - 1)
-        }, 1000)
-    }
-  }, [timeRemaining])
+  function calculateWordCount(text) {
+      const wordsArr = text.trim().split(" ")
+      return wordsArr.filter(word => word !== "").length
+  }
+  
+  function startGame() {
+      setIsTimeRunning(true)
+      setTimeRemaining(STARTING_TIME)
+      setText("")
+  }
+  
+  function endGame() {
+      setIsTimeRunning(false)
+      setWordCount(calculateWordCount(text))
+  }
 
-  console.log(words)
+  
+  useEffect(() => {
+      if(isTimeRunning && timeRemaining > 0) {
+          setTimeout(() => {
+              setTimeRemaining(time => time - 1)
+          }, 1000)
+      } else if(timeRemaining === 0) {
+          endGame()
+      }
+  }, [timeRemaining, isTimeRunning])
+  
   return (
       <div>
           <h1>How fast do you type?</h1>
-          <textarea 
-              value={words}
-              name="words"
-              placeholder="Type in..."
+          <textarea
               onChange={handleChange}
+              value={text}
+              disabled={!isTimeRunning}
           />
-          <h4>Time reminaing: {timeRemaining}</h4>
-          <button onClick={() => console.log(calculateWordCount(words))}>Start</button>
-          <h1>Word count: ???</h1>
+          <h4>Time remaining: {timeRemaining}</h4>
+          <button 
+              onClick={startGame}
+              disabled={isTimeRunning}
+          >
+              Start
+          </button>
+          <h1>Word count: {wordCount}</h1>
       </div>
   )
 }
