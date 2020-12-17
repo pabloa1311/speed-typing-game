575ad894af1b744825947e59e4a4b5ee3c4478cc
pabloa1311 <pabloa1311@gmail.com>
2020-12-17 18:48:42 -0500

    Final version with all the hooks

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
-import React, {useState, useEffect, useRef} from "react"
+import React from "react"
+import useFunctions from "./useFunctions"
 
 function App() {
-    const STARTING_TIME = 10
     
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    const textBoxRef = useRef(null)
-    
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
-    }
-    
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-    
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-        textBoxRef.current.disabled = false
-        textBoxRef.current.focus()
-    }
-    
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-    
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            endGame()
-        }
-    }, [timeRemaining, isTimeRunning]) // eslint-disable-line react-hooks/exhaustive-deps
+    const {textBoxRef, handleChange, text, isTimeRunning, timeRemaining, startGame, wordCount} = useFunctions()
     
     return (
         <div>
-            <h1>Speed Typing Game</h1>
+            <h1>How fast do you type?</h1>
             <textarea
                 ref={textBoxRef}
                 onChange={handleChange}
                 value={text}
                 disabled={!isTimeRunning}
-                placeholder="How fast do you type?"
             />
             <h4>Time remaining: {timeRemaining}</h4>
             <button 
